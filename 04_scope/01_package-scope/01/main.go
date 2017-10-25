2ed93acc1aa577219de2a687116d18903e2ba2d9
dashafri <32595865+dashafri@users.noreply.github.com>
2017-10-25 17:33:59 +0800

    visibility of packages

--- 04_scope/01_package-scope/01/main.go
+++ 04_scope/01_package-scope/01/main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 var x int = 42
+
 // scope of x since it is not in curly braces is of the whole package
 // i.e. package level scope
 
@@ -15,13 +16,18 @@ func main() {
 
 func poo() {
 	fmt.Println("1")
-	loo()
+	boo()
 }
 
 func boo() {
 	fmt.Println("2")
+	y := 17
+	fmt.Println(y)
+
 }
 
-func loo(){
+func loo() {
 	fmt.Println("3")
 }
+
+//between the braces block level scope enclosed in larger scope area
---
+++ 04_scope/01_package-scope/02_visibility/vis/name.go
@@ -0,0 +1,4 @@
+package vis
+
+var MyName = "Danial"
+var yourName = "Future Rock Star Programmer"
